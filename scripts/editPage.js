7048329ee7be9a647d906aa0ebb9900e095a1c50
amso95 <amanda-solberg@hotmail.com>
2024-03-28 14:24:54 +0100

    Transfered project from IntelliJ to VSCode

---
+++ scripts/editPage.js
@@ -0,0 +1,33 @@
+const home = document.getElementById("home");
+const homePage = document.getElementById("homePage");
+
+homePage.addEventListener("click",
+    function() {
+        window.location = "homePage.html";
+    }
+);
+home.addEventListener("click",
+    function() {
+        window.location = "homePage.html";
+    }
+);
+
+function httpGet(){
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://localhost:8080/blog-post?id=1");
+      xhr.send();
+      xhr.responseType = "json";
+      xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          console.log(xhr.response);
+          document.getElementById("testP").innerHTML = "<div style='margin-top:10px'><h2>" + xhr.response.title + "</h2>" +
+                                                        "<p>" + xhr.response.textContent + "</p>"+
+                                                        "<p>Date: " + xhr.response.date + ", id: " +
+                                                        xhr.response.id + ", userId: " + xhr.response.userId
+                                                        + "</p></div>";
+        }
+        else {
+          console.log(`Error: ${xhr.status}`);
+        }
+    };
+}
---
+++ scripts/homePage.js
@@ -0,0 +1,42 @@
+const edit = document.getElementById("edit");
+const homePage = document.getElementById("homePage");
+const create = document.getElementById("create");
+
+homePage.addEventListener("click",
+    function() {
+        window.location = "homePage.html";
+    }
+);
+edit.addEventListener("click",
+    function() {
+        window.location = "editpage.html";
+    }
+);
+create.addEventListener("click",
+    function() {
+        console.log("test");
+        window.location = "createpage.html";
+    }
+);
+
+function getAllPosts() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8080/blog-post/get-all-posts");
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            for(let i = 0; i < xhr.response.length; i++){
+                document.getElementById("blogPosts").innerHTML += "<h3>" + xhr.response[i].title + "</h3><br>" +
+                                                                   "<p>" + xhr.response[i].textContent + "</p><br>" +
+                                                                   "<p>" + "Date: " + xhr.response[i].date +
+                                                                   ", ID: " + xhr.response[i].id +
+                                                                   ", userId: " + xhr.response[i].userId + "</p><br><br>";
+            }
+        }
+        else {
+          console.log(`Error: ${xhr.status}`);
+          document.getElementById("blogPosts").innerHTML = "Error something went wrong!"
+        }
+    };
+}
