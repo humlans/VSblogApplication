242519a37f862e1e95e1b2fc90e21fcc9f86b1ea
amso95 <amanda-solberg@hotmail.com>
2024-04-08 12:24:31 +0200

    Updated delete button to make a reload after post got deleted

--- scripts/homePageAdmin.js
+++ scripts/homePageAdmin.js
@@ -1,30 +1,33 @@
 const userid = sessionStorage.getItem("userid");
-if(userid != null){    
-    // If userId is send from login page use that id the get that users posts.
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost:8080/blog-post/get-all-posts");
-    xhr.send();
-    xhr.responseType = "json";
-    xhr.onload = () => {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById("blogPosts").innerHTML = "";
-            for(let i = 0; i < xhr.response.length; i++){
-                if(xhr.response[i].userId == userid){
-                    document.getElementById("blogPosts").innerHTML += "<div class='editPostDiv'><h3 class='postTitle'>" + xhr.response[i].title + "</h3><br>" +
-                                                                    "<p class='textContent'>" + xhr.response[i].textContent + "</p><br>" +
-                                                                    "<p class='dateContent'>" + "Date: " + xhr.response[i].date +
-                                                                    ", ID: " + xhr.response[i].id +
-                                                                    ", userId: " + xhr.response[i].userId + "</p>"+
-                                                                    "<div class='buttonsDiv'><button onclick='goToEditPost("+ xhr.response[i].id + ")'>Edit</button>"+
-                                                                    "<button onclick='deletePost("+ xhr.response[i].id + ")'>Delete</button></div></div><br><br>";
-            }
-        }
-        } else {
-            console.log(`Error: ${xhr.status}`);
-        }
-    };
-}
+getLoggedInUsersPosts();
 
+function getLoggedInUsersPosts(){
+    if(userid != null){    
+        // If userId is send from login page use that id the get that users posts.
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost:8080/blog-post/get-all-posts");
+        xhr.send();
+        xhr.responseType = "json";
+        xhr.onload = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById("blogPosts").innerHTML = "";
+                for(let i = 0; i < xhr.response.length; i++){
+                    if(xhr.response[i].userId == userid){
+                        document.getElementById("blogPosts").innerHTML += "<div class='editPostDiv'><h3 class='postTitle'>" + xhr.response[i].title + "</h3><br>" +
+                                                                        "<p class='textContent'>" + xhr.response[i].textContent + "</p><br>" +
+                                                                        "<p class='dateContent'>" + "Date: " + xhr.response[i].date +
+                                                                        ", ID: " + xhr.response[i].id +
+                                                                        ", userId: " + xhr.response[i].userId + "</p>"+
+                                                                        "<div class='buttonsDiv'><button onclick='goToEditPost("+ xhr.response[i].id + ")'>Edit</button>"+
+                                                                        "<button onclick='deletePost("+ xhr.response[i].id + ")'>Delete</button></div></div><br><br>";
+                }
+            }
+            } else {
+                console.log(`Error: ${xhr.status}`);
+            }
+        };
+    }
+}
 function goToEditPost(postId) {
     sessionStorage.setItem("id", postId);
     sessionStorage.setItem("userid", userid);
