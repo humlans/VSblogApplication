f5f51386aa5c422a5911da296a9169da71d47ab3
humlans <spangbergwilma@gmail.com>
2024-04-12 16:24:10 +0200

    Added pageable and buttons for it

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -33,45 +33,72 @@ sortPosts.addEventListener("click", applySort);
 function applySort(event) {
     // Prevents the default form submission behavior.
     event.preventDefault();
-    getAllPosts(event); 
+    clickLoadPageFunction(0);
+    createButtonsForLoadNextPosts();
 }
+
+function createButtonsForLoadNextPosts () {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:8080/blog-post/get-number-of-posts", true);
+    xhr.responseType = "json";
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            let amount = Math.floor(xhr.response/5)
+            if(xhr.response%5 != 0) {
+                amount += 1
+            }
+
+            const choosePageList = document.getElementById("choosePageList");
+            choosePageList.innerHTML = "";
+            for(let i = 0; i < amount; i++){
+                choosePageList.innerHTML += "<li class='pagesElement'><button id='pageButton' value='" + i + "'>" + (i+1) + "</button></li>";
+            }
+        }
+        else{
+            console.log("Error:" + xhr.status);
+            document.getElementById("blogPosts").innerHTML = "Error something went wrong!";
+        }
+    }
+}
+
+// Function to move between pages of posts with buttons.
+const nextPageDiv = document.getElementById("nextPageDiv");
+nextPageDiv.addEventListener("click", function(event) {
+    let target = event.target; 
+    clickLoadPageFunction(target.value);    
+});
+
+// Function to sort correctly by calling the database.
 // Fetches and displays posts with a general sorting criteria using an AJAX request.
-function getAllPosts(event) {
+function clickLoadPageFunction (pageNumber) {
     const sortBy = document.getElementById('sortSelect').value;
     if(sortBy == ""){
          // Defaults to sorting by the newest date if no sort option is selected.
         sortBy = 'dateNewest';
     }
-    event.preventDefault();
+
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost:8080/blog-post/get-all-posts-userId", true);
+    xhr.open("GET", "http://localhost:8080/blog-post/get-all-posts-by-page?sortBy=" + sortBy + "&page=" + pageNumber, true);
     xhr.responseType = "json";
     xhr.send();
     xhr.onload = () => {
         if (xhr.readyState == 4 && xhr.status == 200) {
             let posts = xhr.response;
-            // Sort posts by date
-            if (sortBy === 'dateNewest') {
-                posts.sort((a, b) => new Date(b.date) - new Date(a.date));
-            } 
-            else if (sortBy === 'dateOldest') {
-                posts.sort((a, b) => new Date(a.date) - new Date(b.date));
-            }
+
             // Clear existing posts
             document.getElementById("blogPosts").innerHTML = '';
-            // Iterates through each post and adds it to the display element.
+            
             for(let i = 0; i < posts.length; i++){            
-                if  ( xhr.readyState == 4 && xhr.status == 200){
                     document.getElementById("blogPosts").innerHTML += '<div class="blogPostDiv"> <h3 class="postTitle">' + posts[i].title + '</h3><br>' +
                     '<p class="textContent">' + posts[i].textContent + '</p><br>' +
                     '<p class="postUsername">' + 'Posted by: ' + posts[i].user.userName + '</p>' +
                     '<p class="dateContent">' + 'Date: ' + posts[i].date + '</div>';
-                }
-                else {
-                    console.log("Error:" + xhr.status);
-                    document.getElementById("blogPosts").innerHTML = "Error something went wrong!";
-                }
             }           
         }
-    };
+        else{
+            console.log("Error:" + xhr.status);
+            document.getElementById("blogPosts").innerHTML = "Error something went wrong!";
+        }
+    }
 }
